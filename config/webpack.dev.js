0b4d3693b33cec923977b252547ba94e09439636
sash-ua <from.usa.4you@gmail.com>
2017-09-06 13:41:10 +0300

    0.0.2

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/',
+    publicPath: '',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
